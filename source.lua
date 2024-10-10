96eac5ea0fb121f3e2de0eb97f3979fd7e4ae30c
BrightCat14 <107373258+BrightCat14@users.noreply.github.com>
2024-10-10 19:57:30 +0300

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,16 +1,30 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "Eternity", HidePremium = false, SaveConfig = true, ConfigFolder = "Eternity"})
+
+local player = game.Players.LocalPlayer
+local playername = player.Name
+local character = player.Character or player.CharacterAdded:Wait()
+
 local Combat = Window:MakeTab({
 	Name = "Combat",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
 OrionLib:MakeNotification({
-	Name = "Title!",
-	Content = "Notification content... what will it say??",
+	Name = "Eternity",
+	Content = "Hi, " .. playername .. ", glad to see you",
 	Image = "rbxassetid://4483345998",
 	Time = 5
 })
+Combat:AddButton({
+	Name = "Reach",
+	Callback = function()
+			local equippedTool = character:FindFirstChildOfClass("Tool")
+			equippedTool.Glove.Transparency = 0.35
+			equippedTool.Glove.Size = Vector3.new(25, 25, 25)
+  	end    
+})
+
 
 OrionLib:Init()
 -- destroying the interface: OrionLib:Destroy()
